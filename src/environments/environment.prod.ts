9ec39c502a92c88425326280aae2377fba728ee3
farooq.khan <farooqk.work@gmail.com>
2018-10-22 16:03:03 +0500

    added docs folder for github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   component_events: {
-    init_file_upload: 'init-file-upload'
+    INIT_FILE_UPLOAD: 'init-file-upload',
+    FILE_UPLOAD_COMPLETE: 'upload-complete'
   },
   firebase: {
-    app_name: 'fileuploader',
     config: {
       apiKey: "AIzaSyCLVv73LEv5FftuH7P31XKQktT4WWYCF7Y",
       authDomain: "fileuploader-97e91.firebaseapp.com",
@@ -13,6 +13,7 @@ export const environment = {
       storageBucket: "fileuploader-97e91.appspot.com",
       messagingSenderId: "936995345942"
     },
-    folder: 'gs://fileuploader-97e91.appspot.com'
+    project_storage_url: 'gs://fileuploader-97e91.appspot.com',
+    base_folder: 'file-uploader-data/'    
   }
 };
